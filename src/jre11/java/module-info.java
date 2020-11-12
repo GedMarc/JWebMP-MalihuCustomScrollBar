931f30a12ab0c038c0c434e22272aeaff52809ea
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:39:16 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -4,7 +4,7 @@ module com.jwebmp.plugins.malihuscrollbar {
 	exports com.jwebmp.plugins.malihu.options;
 
 	requires com.jwebmp.core;
-	requires java.validation;
+	requires jakarta.validation;
 	requires com.fasterxml.jackson.databind;
 
 	opens com.jwebmp.plugins.malihu to com.fasterxml.jackson.databind, com.google.guice, com.jwebmp.core;
